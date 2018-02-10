eda6779f9101375afa75d61291aa6125fc83970b
Job Lindsen <Job.Lindsen@avantiagroup.com>
2018-02-10 15:54:50 +0000

    update ip address zookeeper

--- src/main/java/myapps/ModelTest.java
+++ src/main/java/myapps/ModelTest.java
@@ -38,7 +38,8 @@ public class ModelTest {
 		// Configure Kafka Streams Application
         Properties props = new Properties();
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-modeltest");
-        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        //props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "http://35.229.35.186:9092");
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 		
